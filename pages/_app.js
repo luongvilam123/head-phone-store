4ebe3450e557daadde9b16e0743ba3edbf2f3dd3
LuongViLam <kulam1782000@gmail.com>
2022-05-03 11:04:35 +0700

    Context logic for website

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,19 @@
 import '../styles/globals.css'
 import React from 'react'
-import {Layout} from "../components"
+import {Layout} from "../components";
+import {StateContext} from "../context/StateContext";
+import {Toaster} from "react-hot-toast"
 
 function MyApp({ Component, pageProps }) {
   return(
-
+   <StateContext>
     <Layout>
         <Component {...pageProps} />
-  </Layout>
-
-    ) 
+    </Layout>
+   </StateContext>
+) 
 }
+
+
 
 export default MyApp
