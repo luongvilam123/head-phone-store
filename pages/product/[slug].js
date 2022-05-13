bf1faf44c0bbecb0347f31b419d8be95c57f2c84
LuongViLam <kulam1782000@gmail.com>
2022-05-13 19:48:22 +0700

    Done

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -8,8 +8,12 @@ const ProductDetails = ({product, products}) => {
 
     const {image, name, details, price} = product;
     const[index,setIndex]= useState(0)
-    const {decQty, incQty,qty, onAdd} =useStateContext();
+    const {decQty, incQty,qty, onAdd,setShowCart} =useStateContext();
 
+    const handleBuyNow=()=>{
+        onAdd(product,qty);
+        setShowCart(true);
+    }
   return (
     <div>
     
@@ -56,6 +60,7 @@ const ProductDetails = ({product, products}) => {
                                  onClick={ () => onAdd(product, qty) }> Add to Cart</button>
                          <button type='button' 
                                  className='buy-now'
+                                 onClick={handleBuyNow}
                                  > Buy Now</button>
                     </div>
              </div>
--- pages/success.js
+++ pages/success.js
@@ -1,13 +1,19 @@
 import React, {useEffect,useState} from 'react';
 import Link from 'next/link';
 import {BsBagCheckFill} from 'react-icons/bs';
-
+import {runFireworks} from "../lib/utils"
 import { useStateContext } from '../context/StateContext';
 
 const Success = () => {
 
     const {setCartItems,setTotalPrice,setTotalQuantities}=useStateContext();
-    
+    useEffect(()=>{
+        localStorage.clear();
+        setCartItems([]);
+        setTotalPrice(0);
+        setTotalQuantities(0);
+        runFireworks();
+    },[])
   return (
     <div className='success-wrapper'>
         <div className='success'>
@@ -15,6 +21,14 @@ const Success = () => {
                 <BsBagCheckFill />
             </p>
             <h2>Thank you for you order !</h2>
+            <p className='email-msg'> Check your email inbox for the receipt</p>
+            <p className='description'>
+                if you have any questions please email
+                <a className='email' href='mailto:lamluongit2000@gmail.com'>lamluongit2000@gmail.com</a>
+            </p>
+            <Link href={"/"}>
+                <button type='button' width="300px" className='btn'> Continue shopping</button>
+            </Link>
         </div>
     </div>
   )
